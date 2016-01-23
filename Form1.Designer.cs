e6862ef606232828388c0d9dc05182ef504bdebe
davin8000 <cs.davin@gmail.com>
2016-01-23 21:55:07 +0800

    Added Tooltip
    Added Tooltip

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,6 +42,13 @@
             this.button4 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -57,7 +64,8 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(103, 17);
+            this.label1.Location = new System.Drawing.Point(103, 12);
+            this.label1.MaximumSize = new System.Drawing.Size(110, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 1;
@@ -67,14 +75,13 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(106, 42);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.Size = new System.Drawing.Size(54, 20);
             this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Location = new System.Drawing.Point(9, 45);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 13);
             this.label2.TabIndex = 3;
@@ -127,7 +134,8 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(103, 105);
+            this.label5.Location = new System.Drawing.Point(103, 100);
+            this.label5.MaximumSize = new System.Drawing.Size(110, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(104, 13);
             this.label5.TabIndex = 11;
@@ -147,21 +155,94 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.HorizontalScrollbar = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 158);
+            this.listBox1.Location = new System.Drawing.Point(12, 171);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(194, 134);
+            this.listBox1.Size = new System.Drawing.Size(116, 121);
             this.listBox1.TabIndex = 12;
+            this.listBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseMove);
             // 
             // notifyIcon1
             // 
             this.notifyIcon1.Text = "Root Copier";
             this.notifyIcon1.Visible = true;
             // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.Location = new System.Drawing.Point(134, 171);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(73, 95);
+            this.listBox2.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Location";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(131, 155);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Filter";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(166, 40);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(41, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Add";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(134, 269);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(73, 23);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Remove";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 295);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Status:  ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(51, 295);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 19;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(218, 302);
+            this.ClientSize = new System.Drawing.Size(218, 325);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button4);
@@ -199,6 +280,13 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,8 @@ namespace RootCopier
     public partial class Form1 : Form
     {
         IList<string> files = new List<string>();
+        ToolTip toolTip1 = new ToolTip();
+        int index = -1;
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +24,7 @@ namespace RootCopier
         private void Form1_Load(object sender, EventArgs e)
         {
             updateGUI(0);
+            updateStatus("Waiting for Root Directory", Color.Red);
         }
 
         private void updateGUI(int state)
@@ -30,35 +33,51 @@ namespace RootCopier
             {
                 case 0:
                     textBox1.Enabled = false;
+                    button5.Enabled = false;
+                    button6.Enabled = false;
                     button2.Enabled = false;
                     button3.Enabled = false;
                     button4.Enabled = false;
                     break;
                 case 1:
                     textBox1.Enabled = true;
+                    button5.Enabled = true;
+                    button6.Enabled = false;
                     button2.Enabled = false;
                     button3.Enabled = false;
                     button4.Enabled = false;
                     break;
                 case 2:
                     textBox1.Enabled = true;
+                    button5.Enabled = true;
+                    button6.Enabled = true;
                     button2.Enabled = true;
                     button3.Enabled = false;
                     button4.Enabled = false;
                     break;
                 case 3:
                     textBox1.Enabled = true;
+                    button5.Enabled = true;
+                    button6.Enabled = true;
                     button2.Enabled = true;
                     button3.Enabled = true;
                     button4.Enabled = false;
                     break;
                 case 4:
                     textBox1.Enabled = true;
+                    button5.Enabled = true;
+                    button6.Enabled = true;
                     button2.Enabled = true;
                     button3.Enabled = true;
                     button4.Enabled = true;
                     break;
             }
+        }
+
+        private void updateStatus(string text,Color textColor)
+        {
+            label9.Text = text;
+            label9.ForeColor = textColor;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -69,16 +88,9 @@ namespace RootCopier
                 {
                     label1.Text = fbd.SelectedPath;
                     updateGUI(1);
+                    updateStatus("Path valid, waiting for file filters", Color.Green);
                 }
             }
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            if (!(string.Compare(textBox1.Text, "", false) == 0))
-                updateGUI(2);
-            else
-                updateGUI(1);
         }
 
         private static long DirSize(DirectoryInfo d) //Recursive
@@ -97,14 +109,18 @@ namespace RootCopier
             long parsedSize = 0;
             var stack = new Stack<string>();
             var node = "";
-            var pattern = "";
+            var dir = "";
+            IList<string> pattern = new List<string>();
             if (InvokeRequired) //access gui on another thread
                 Invoke(new MethodInvoker(() =>
                 {
-                    rootSize = DirSize(new DirectoryInfo(label1.Text));
-                    pattern = textBox1.Text;
+                    dir = label1.Text;
+                    foreach (string filter in listBox2.Items) pattern.Add(filter);
                     node = label1.Text;
+                    updateStatus("Calculating folder size...", Color.Red);
                 }));
+            rootSize = DirSize(new DirectoryInfo(dir));
+            if (InvokeRequired) Invoke(new MethodInvoker(() => updateStatus("Search for files...", Color.Red)));
             stack.Push(node);
             while (stack.Count > 0)
             {
@@ -118,11 +134,13 @@ namespace RootCopier
                 {
                     FileInfo file = new FileInfo(dirFiles);
                     parsedSize += file.Length;
-                    if (string.Compare(file.Extension, string.Concat(".", pattern), true) == 0)
-                    {
-                        files.Add(dirFiles);
-                        if (InvokeRequired) Invoke(new MethodInvoker(() => listBox1.Items.Add(dirFiles)));
-                    }
+                    foreach (string filter in pattern)
+                        if (string.Compare(file.Extension, string.Concat(".", filter), true) == 0)
+                        {
+                            files.Add(dirFiles);
+                            if (InvokeRequired) Invoke(new MethodInvoker(() => listBox1.Items.Add(Path.GetFileName(dirFiles))));
+                            break;
+                        }
                     if (InvokeRequired) Invoke(new MethodInvoker(() => progressBar1.Value = ((int)Math.Round((double)parsedSize / (double)rootSize * 100))));
                 }
             }
@@ -132,9 +150,14 @@ namespace RootCopier
         {
             progressBar1.Value = 0;
             files.Clear();
+            listBox1.Items.Clear();
+            listBox1.Items.Clear();
+            listBox1.Items.Clear();
             await Task.WhenAll(Task.Factory.StartNew(() => seachProcess()));
             label4.Text = files.Count.ToString();
             updateGUI(3);
+            updateStatus(@"Search Done, 
+waiting for valid output folder", Color.Green);
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -145,6 +168,8 @@ namespace RootCopier
                 {
                     label5.Text = fbd.SelectedPath;
                     updateGUI(4);
+                    updateStatus(@"Output folder valid, 
+waiting for operation", Color.Green);
                 }
             }
         }
@@ -154,9 +179,9 @@ namespace RootCopier
             for (int i = 0; i < files.Count; i++)
             {
                 string str = files[i];
-                FileStream fileStream = new FileStream(str, FileMode.Open);
+                FileStream fileStream = new FileStream(str, FileMode.Open, FileAccess.Read);
                 FileStream fileStream1 = null;
-                if (InvokeRequired) Invoke(new MethodInvoker(() => fileStream1 = new FileStream(Path.Combine(label5.Text, Path.GetFileName(str)), FileMode.Create)));
+                if (InvokeRequired) Invoke(new MethodInvoker(() => fileStream1 = CreateFileWithUniqueName(label5.Text, Path.GetFileName(str), int.MaxValue)));
                 long num = (fileStream.Length - 1);
                 byte[] numArray = new byte[1025];
                 while (fileStream.Position < num)
@@ -171,15 +196,74 @@ namespace RootCopier
             }
         }
 
+        private FileStream CreateFileWithUniqueName(string folder, string fileName, int maxAttempts)
+        {
+            var fileBase = Path.GetFileNameWithoutExtension(fileName);
+            var ext = Path.GetExtension(fileName);
+            var files = new HashSet<string>(Directory.GetFiles(folder));
+            for (var index = 0; index < maxAttempts; index++)
+            {
+                var name = (index == 0) ? fileName : String.Format("{0} ({1}){2}", fileBase, index, ext);
+                var fullPath = Path.Combine(folder, name);
+                if (files.Contains(fullPath)) continue;
+                try
+                {
+                    return new FileStream(fullPath, FileMode.CreateNew, FileAccess.Write);
+                }
+                catch (DirectoryNotFoundException) { throw; }
+                catch (DriveNotFoundException) { throw; }
+                catch (IOException) { } // ignore this and try the next filename
+            }
+            throw new Exception("Could not create unique filename in " + maxAttempts + " attempts");
+        }
+
         private async void button4_Click(object sender, EventArgs e)
         {
             progressBar1.Value = 0;
+            updateStatus("Copy started, copying files...", Color.Red);
             await Task.WhenAll(Task.Factory.StartNew(() => copyFiles()));
+            updateStatus("Copy done!", Color.Green);
             notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
             notifyIcon1.BalloonTipTitle = "Done!";
-            notifyIcon1.BalloonTipText = string.Concat("Done copying: ", files.Count.ToString(), " ", textBox1.Text, " files");
+            notifyIcon1.BalloonTipText = string.Concat("Done copying: ", files.Count.ToString(), " ", listBox2.Items.ToString(), " files");
             notifyIcon1.Icon = SystemIcons.Application;
             notifyIcon1.ShowBalloonTip(5);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!listBox2.Items.Contains(textBox1.Text)) listBox2.Items.Add(textBox1.Text);
+            if (Convert.ToBoolean(listBox2.Items.Count)) updateGUI(2);
+            else updateGUI(1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try { listBox2.Items.RemoveAt(listBox2.SelectedIndex); } catch { } //ignored outofrange
+            if (Convert.ToBoolean(listBox2.Items.Count)) updateGUI(2);
+            else updateGUI(1);
+        }
+
+        private void listBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (files.Count > 0)
+            {
+                int newindex = listBox1.IndexFromPoint(e.Location);
+                if (newindex >= 0 && newindex < files.Count)
+                {
+                    if (newindex != index) //avoid flickering
+                    {
+                        string tip = files[newindex];
+                        toolTip1.AutoPopDelay = 5000;
+                        toolTip1.InitialDelay = 1000;
+                        toolTip1.ReshowDelay = 500;
+                        toolTip1.ShowAlways = true;
+                        toolTip1.SetToolTip(listBox1, tip);
+                        index = newindex;
+                    }
+                }
+                else toolTip1.Hide(listBox1);
+            }
         }
     }
 }
